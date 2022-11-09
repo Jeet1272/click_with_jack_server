535e7985efed5734c3304d348e999d6917ab2b77
Jeet1272 <bishwajeetsarkarbd@gmail.com>
2022-11-10 01:58:26 +0600

    Create operation done

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+app.use(express.json())
+
+// pass: CYLymhlZcfkWdNUD
+
+
+
+const uri = "mongodb+srv://click-with-jack:CYLymhlZcfkWdNUD@cluster0.f7pydfc.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        const servicesCollection = client.db('click-with-jack').collection('services')
+        app.post('/services', async (req, res) => {
+            const service = req.body;
+
+            const result = await servicesCollection.insertOne(service);
+            res.send(result)
+        })
+    }
+    finally {
+
+    }
+}
+run().catch((err) => console.error(err));
+
 
 app.get('/', (req, res) => { res.send('Hello world from click with jack server') });
 
