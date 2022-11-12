be6ae130f45ae5b8cb1d9a1790b9b4f3aed285c2
Jeet1272 <bishwajeetsarkarbd@gmail.com>
2022-11-12 22:14:06 +0600

    details api created

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
 
@@ -23,6 +23,15 @@ async function run() {
             const services = await cursor.toArray();
             res.send(services)
         })
+
+        app.get('/service/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) };
+            const service = await servicesCollection.findOne(query)
+            res.send(service)
+        })
+
+
 
         app.get('/services/limit', async (req, res) => {
             const query = {};
