7c069a87a97c92de88e775f3269cb1b45d8a9721
Jeet1272 <bishwajeetsarkarbd@gmail.com>
2022-11-13 21:00:24 +0600

    env added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
@@ -7,11 +8,10 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json())
 
-// pass: CYLymhlZcfkWdNUD
 
 
 
-const uri = "mongodb+srv://click-with-jack:CYLymhlZcfkWdNUD@cluster0.f7pydfc.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.f7pydfc.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
     try {
